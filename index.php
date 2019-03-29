327298ff9e87a10110a3954addedc99a7b7abb31
cetecom <cetecom@LCV10412>
2019-03-29 15:53:38 -0300

    Colores de fondo

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('lib/constantes.php')?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -7,11 +8,19 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
         <title></title>
     </head>
     <body>
+        <div id="Logo">
+            <div id="titulo"></div>
+            <div id="tituloPagina">
+            <div id="contenido"></div>
+        </div>
         <?php
-        // put your code here
+            var_dump(session_id());
+            $_SESSION["hm"]="Hola Mundo";
+            ?>
         ?>
     </body>
 </html>
